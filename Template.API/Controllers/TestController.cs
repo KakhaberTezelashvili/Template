3d8f6fb2aefa73e6e7bd11f3ac87df6fb9b2a33b
KakhaberTezelashvili <kakha.tezelashvili@singular.uk>
2020-07-15 13:53:39 +0400

    Initial Commit

---
+++ Template.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
